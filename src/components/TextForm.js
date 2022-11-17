6a44b7fd055cf5517238eeb8f370e7810d9d18dd
Neeraj Verma <72142904+neeraj-verma-20@users.noreply.github.com>
2022-11-17 19:51:57 +0530

    first-commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked: " + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to uppercase!", "success");
+    };
+
+    const handleloClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lowercase!", "success");
+    };
+
+    const handleClearText = () => {
+        let newText = "";
+        setText(newText);
+        props.showAlert("Text box cleared!", "success");
+    };
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert("Copied to clipboard!", "success");
+    };
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    };
+
+    const handleOnChange = (event) => {
+        // console.log("On Change");
+        setText(event.target.value);
+    };
+
+    const [text, setText] = useState("");
+    return (
+        <>
+            <div
+                className="container"
+                style={{ color: props.mode === "dark" ? "white" : "#042749" }}
+            >
+                <h1 className="mb-4" style={{ fontSize: "1.8rem" }}>
+                    {props.heading}
+                </h1>
+                <div className="mb-3">
+                    <textarea
+                        className="form-control"
+                        value={text}
+                        onChange={handleOnChange}
+                        style={{
+                            backgroundColor: props.mode === "dark" ? "#13466e" : "white",
+                            color: props.mode === "dark" ? "white" : "black",
+                        }}
+                        id="myBox"
+                        rows="8"
+                    ></textarea>
+                </div>
+                <button
+                    disabled={text.length === 0}
+                    className="btn btn-primary mx-1 my-1"
+                    onClick={handleUpClick}
+                >
+                    Convert to Uppercase
+                </button>
+                <button
+                    disabled={text.length === 0}
+                    className="btn btn-primary mx-1 my-1"
+                    onClick={handleloClick}
+                >
+                    Convert to Lowercase
+                </button>
+                <button
+                    disabled={text.length === 0}
+                    className="btn btn-primary mx-1 my-1"
+                    onClick={handleExtraSpaces}
+                >
+                    Remove Extra Spaces
+                </button>
+                <button
+                    disabled={text.length === 0}
+                    className="btn btn-primary mx-1 my-1"
+                    onClick={handleCopy}
+                >
+                    Copy Text
+                </button>
+                <button
+                    disabled={text.length === 0}
+                    className="btn btn-primary mx-1 my-1"
+                    onClick={handleClearText}
+                >
+                    Clear Text
+                </button>
+            </div>
+            <div
+                className="container my-4"
+                style={{ color: props.mode === "dark" ? "white" : "#042749" }}
+            >
+                <h2>Your text summary</h2>
+                <p>
+                    {
+                        text.split(/\s+/).filter((element) => {
+                            return element.length !== 0;
+                        }).length
+                    }{" "}
+                    Words and {text.length} Characters
+                </p>
+                <p>
+                    {0.008 *
+                        text.split(" ").filter((element) => {
+                            return element.length !== 0;
+                        }).length}{" "}
+                    Minutes Read
+                </p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
+            </div>
+        </>
+    );
+}
